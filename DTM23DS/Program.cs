bd5da55c51ea342fb9e14da9a8db709b0a02d469
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-02-13 17:14:13 +0800

    fix bugs

--- DTM23DS/Program.cs
+++ DTM23DS/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using ArmadaTank.DTMParser.Result;
+using ArmadaTank.DTMParser;
 
 namespace DTM23DS
 {
@@ -12,6 +15,15 @@ namespace DTM23DS
             foreach (var item in System.IO.Directory.GetFiles(".","*.dtm", System.IO.SearchOption.AllDirectories))
             {
                 Console.WriteLine("parsing:{0}", item);
+                var sourceCode = File.ReadAllText(item);
+                var lexi = new ArmadaTank.DTMParser.LexicalAnalyzerDTM(sourceCode);
+                var tokens = lexi.Analyze();
+                var parser = new ArmadaTank.DTMParser.LL1SyntaxParserDTM(tokens);
+                var tree = parser.Parse();
+                var dtmObj = tree.GetModel();
+                var bmp = dtmObj.GenerateBitmap(500,500);
+                bmp.RotateFlip(System.Drawing.RotateFlipType.RotateNoneFlipY);
+                bmp.Save(item + ".bmp");
                 ArmadaTank2ThreedDS.DTM23DSParser.Parse(item);
             }
             Console.WriteLine("Done");
